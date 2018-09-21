21eb301fcba0d6a9d5cc0b58169e76ddfc685b44
Elliot Massen <hello@elliotmassen.com>
2018-09-21 14:37:00 +0100

    Home frontend 🏠

--- themes/2018/src/scripts/index.js
+++ themes/2018/src/scripts/index.js
@@ -1,7 +1,27 @@
-window.onload = function() {
-	navigator.serviceWorker.getRegistrations().then(function(registrations) {
-		for(let registration of registrations) {
-			registration.unregister();
+var HackSussex = (function() {
+	return {
+		setup: function() {
+			this.menu();
+			this.removeServiceWorkers();
+		},
+		removeServiceWorkers: function() {
+			navigator.serviceWorker.getRegistrations().then(function(registrations) {
+				for(let registration of registrations) {
+					registration.unregister();
+				}
+			});
+		},
+		menu: function() {
+			// Mobile menu open/close functionality
+			var header = document.querySelector(".header");
+            var menuButton = document.getElementById("menu-button");
+            menuButton.addEventListener("click", function() {
+                menuButton.parentElement.parentElement.classList.toggle("menu-open");
+            });
 		}
-	});
+	}
+})();
+
+window.onload = function() {
+	HackSussex.setup();
 };
